227f3651645f93a766bc1e9fe925e577771cf927
SherrenKalalo <s2200043@student.unklab.ac.id>
2022-11-30 07:56:10 +0800

    modify about

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import "../assets/css/templatemo-style.css";
+import Zoom from 'react-reveal/Zoom'
 
 export default function About() {
   return (
+    <Zoom>
     <div id='about'>
         <main>
   <header className="row tm-welcome-section">
@@ -69,6 +71,36 @@ export default function About() {
           </figcaption>
         </figure>
       </article>
+      <article className="col-lg-6">
+        <figure className="tm-person">
+          <img src="img/about-05.jpg" alt="Image" className="img-fluid tm-person-img" />
+          <figcaption className="tm-person-description">
+            <h4 className="tm-person-name">Stephen Timothy Lobbu</h4>
+            <p className="tm-person-title">Chef</p>
+            <p className="tm-person-about">Informatics, Computer Science.</p>
+            <div>
+              <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
+              <a href="https://instagram.com" className="tm-social-link"><i className="fab fa-instagram tm-social-icon" /></a>
+            </div>
+          </figcaption>
+        </figure>
+      </article>
+      <article className="col-lg-6">
+        <figure className="tm-person">
+          <img src="img/about-06.jpg" alt="Image" className="img-fluid tm-person-img" />
+          <figcaption className="tm-person-description">
+            <h4 className="tm-person-name">Yazel Stepano Tangkeallo</h4>
+            <p className="tm-person-title">Chef</p>
+            <p className="tm-person-about">Informatics, Computer Science.</p>
+            <div>
+              <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
+              <a href="https://twitter.com" className="tm-social-link"><i className="fab fa-twitter tm-social-icon" /></a>
+              <a href="https://instagram.com" className="tm-social-link"><i className="fab fa-instagram tm-social-icon" /></a>
+              <a href="https://youtube.com" className="tm-social-link"><i className="fab fa-youtube tm-social-icon" /></a>
+            </div>
+          </figcaption>
+        </figure>
+      </article>
     </div>
   </div>
   <div className="tm-container-inner tm-featured-image">
@@ -82,6 +114,6 @@ export default function About() {
   </div>
   
         </main>
-    </div>
+    </div></Zoom>
   )
 }
