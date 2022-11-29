c4920a672b25395d5142645dc3e89a16bb7e53dd
Gregorius Vincent Adam <adamgregvin@gmail.com>
2022-11-30 04:17:32 +0800

    modifikasi Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -67,28 +67,14 @@ export default function Contact() {
             </div>
             <div className="tm-container-inner-2 tm-info-section">
             <div className="row">
-                {/* FAQ */}
                 <div className="col-12 tm-faq">
                 <h2 className="text-center tm-section-title">Denver Group</h2>
-                <p className="text-center">Final Project Front End Web Developer - A</p>
-                <div className="tm-accordion">
-                    <button className="accordion">1. Fusce eu lorem et dui #09C maximus varius?</button>
-                    <div className="panel">
-                    <p>#666 Duis blandit purus vel nenenatis rutrum. Pellentesque pellentesque tindicunt lorem, ac egestas massa sollicitudin vel. Nam scelerisque vulputate quam mollis pretium. Morbi condimentum volutpat.</p>
-                    </div>
-                    <button className="accordion">2. Vestibulum #999 ante ipsum primis in faucibus orci?</button>
-                    <div className="panel">
-                    <p>Mauris euismod odio at commodo rhoncus. Maecenas nec interdum purus, sed auctor est. Sed eleifend urna nec diam consectetur, a aliquet turpis facilisis. Integer est sapien, sagittis vel massa vel, interdum euismod erat. Aenean sollicitudin nisi neque, efficitur posuere urna rutrum porta.</p>
-                    </div>
-                    <button className="accordion">3. Can I redistribute this template as a ZIP file?</button>
-                    <div className="panel">
-                    <p>Redistributing this template as a downloadable ZIP file on any template collection site is strictly prohibited. You will need to <a href="https://templatemo.com/contact">contact TemplateMo</a> for additional permissions about our templates. Thank you.</p>
-                    </div>
-                    <button className="accordion">4. Ut ac erat sit amet neque efficitur faucibus et in lectus?</button>
-                    <div className="panel">
-                    <p>Vivamus viverra pretium ultricies. Praesent feugiat, sapien vitae blandit efficitur, sem nulla venenatis nunc, vel maximus ligula sem a sem. Pellentesque ligula ex, facilisis ac libero a, blandit ullamcorper enim.</p>
-                    </div>
-                </div>	
+                <p className="text-center">Final Project - Front End Web Developer - A</p>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+
                 </div>
             </div>
             </div>
