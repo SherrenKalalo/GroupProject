469a89935c0f94d4a83f44454573d0c88abde403
SherrenKalalo <s2200043@student.unklab.ac.id>
2022-11-29 22:03:45 +0800

    add app

---
+++ src/components/About.js
@@ -0,0 +1,132 @@
+import React from 'react'
+import "../assets/css/templatemo-style.css";
+
+export default function About() {
+  return (
+    <div id='about'>
+        <main>
+  <header className="row tm-welcome-section">
+    <h2 className="col-12 text-center tm-section-title">About Simple House</h2>
+    <p className="col-12 text-center">This is about page of simple house template. 
+      You can modify and use this HTML template for your website. Total 3 HTML pages included in this template. Header image has a parallax effect.</p>
+  </header>
+  <div className="tm-container-inner tm-persons">
+    <div className="row">
+      <article className="col-lg-6">
+        <figure className="tm-person">
+          <img src="img/about-01.jpg" alt="Image" className="img-fluid tm-person-img" />
+          <figcaption className="tm-person-description">
+            <h4 className="tm-person-name">Jennifer Soft</h4>
+            <p className="tm-person-title">Founder and CEO</p>
+            <p className="tm-person-about">Vivamus cursus leo nec sem feugiat sagittis.
+              Duis ut feugiat odio, sit amet accumsan
+              odio.</p>
+            <div>
+              <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
+              <a href="https://twitter.com" className="tm-social-link"><i className="fab fa-twitter tm-social-icon" /></a>
+              <a href="https://instagram.com" className="tm-social-link"><i className="fab fa-instagram tm-social-icon" /></a>
+            </div>
+          </figcaption>
+        </figure>
+      </article>
+      <article className="col-lg-6">
+        <figure className="tm-person">
+          <img src="img/about-02.jpg" alt="Image" className="img-fluid tm-person-img" />
+          <figcaption className="tm-person-description">
+            <h4 className="tm-person-name">Daisy Walker</h4>
+            <p className="tm-person-title">Executive Chef</p>
+            <p className="tm-person-about">Praesent non vulputate elit. Orci varius
+              natoque et magnis dis parturient, nascetur ridiculus mus.</p>
+            <div>
+              <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
+              <a href="https://twitter.com" className="tm-social-link"><i className="fab fa-twitter tm-social-icon" /></a>
+            </div>
+          </figcaption>
+        </figure>
+      </article>
+      <article className="col-lg-6">
+        <figure className="tm-person">
+          <img src="img/about-03.jpg" alt="Image" className="img-fluid tm-person-img" />
+          <figcaption className="tm-person-description">
+            <h4 className="tm-person-name">Florence Nelson</h4>
+            <p className="tm-person-title">Kitchen Manager</p>
+            <p className="tm-person-about">Aenean sapien sem, ultricies sed vulputate
+              et, auctor vel mauris. Integer sit amet diam eget est facilisis lacinia vitae.</p>
+            <div>
+              <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
+              <a href="https://instagram.com" className="tm-social-link"><i className="fab fa-instagram tm-social-icon" /></a>
+            </div>
+          </figcaption>
+        </figure>
+      </article>
+      <article className="col-lg-6">
+        <figure className="tm-person">
+          <img src="img/about-04.jpg" alt="Image" className="img-fluid tm-person-img" />
+          <figcaption className="tm-person-description">
+            <h4 className="tm-person-name">Valentina Martin</h4>
+            <p className="tm-person-title">Culinary Director</p>
+            <p className="tm-person-about">Praesent non vulputate elit. Orci varius
+              natoque penatibus et magnis montes, nascetur ridiculus mus.</p>
+            <div>
+              <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
+              <a href="https://twitter.com" className="tm-social-link"><i className="fab fa-twitter tm-social-icon" /></a>
+              <a href="https://instagram.com" className="tm-social-link"><i className="fab fa-instagram tm-social-icon" /></a>
+              <a href="https://youtube.com" className="tm-social-link"><i className="fab fa-youtube tm-social-icon" /></a>
+            </div>
+          </figcaption>
+        </figure>
+      </article>
+    </div>
+  </div>
+  <div className="tm-container-inner tm-featured-image">
+    <div className="row">
+      <div className="col-12">
+        <div className="placeholder-2">
+          <div className="parallax-window-2" data-parallax="scroll" data-image-src="img/about-05.jpg" />		
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="tm-container-inner tm-features">
+    <div className="row">
+      <div className="col-lg-4">
+        <div className="tm-feature">
+          <i className="fas fa-4x fa-pepper-hot tm-feature-icon" />
+          <p className="tm-feature-description">Donec sed orci fermentum, convallis lacus id, tempus elit. Sed eu neque accumsan, porttitor arcu a, interdum est. Donec in risus eu ante.</p>
+          <a href="index.html" className="tm-btn tm-btn-primary">Read More</a>
+        </div>
+      </div>
+      <div className="col-lg-4">
+        <div className="tm-feature">
+          <i className="fas fa-4x fa-seedling tm-feature-icon" />
+          <p className="tm-feature-description">Maecenas pretium rutrum molestie. Duis dignissim egestas turpis sit. Nam sed suscipit odio. Morbi in dolor finibus, consequat nisl eget.</p>
+          <a href="index.html" className="tm-btn tm-btn-success">Read More</a>
+        </div>
+      </div>
+      <div className="col-lg-4">
+        <div className="tm-feature">
+          <i className="fas fa-4x fa-cocktail tm-feature-icon" />
+          <p className="tm-feature-description">Morbi in dolor finibus, consequat nisl eget, pretium nunc. Maecenas pretium rutrum molestie. Duis dignissim egestas turpis sit.</p>
+          <a href="index.html" className="tm-btn tm-btn-danger">Read More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="tm-container-inner tm-history">
+    <div className="row">
+      <div className="col-12">
+        <div className="tm-history-inner">
+          <img src="img/about-06.jpg" alt="Image" className="img-fluid tm-history-img" />
+          <div className="tm-history-text"> 
+            <h4 className="tm-history-title">History of our restaurant</h4>
+            <p className="tm-mb-p">Sed ligula risus, interdum aliquet imperdiet sit amet, auctor sit amet justo. Maecenas posuere lorem id augue interdum vehicula. Praesent sed leo eget libero ultricies congue.</p>
+            <p>Redistributing this template as a downloadable ZIP file on any template collection site is strictly prohibited. You will need to <a href="https://templatemo.com/contact">contact TemplateMo</a> for additional permissions about our templates. Thank you.</p>
+          </div>
+        </div>	
+      </div>
+    </div>
+  </div>
+        </main>
+    </div>
+  )
+}
---
+++ src/components/Homepage.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import "../assets/css/templatemo-style.css";
+import Contact from './Contact';
+import About from './About';
+import axios from "axios";
+import Top from './Top';
+
+const Homepage = () => {
+
+    const [menu, setMenu] = useState([]);
+    useEffect(()=>{
+        axios
+        .get("http://localhost:3004/menu")
+        .then((res) => setMenu (res.data));
+    },[]);
+
+    return(
+        <div className="container">
+            <Top />
+            <main>
+                <header className="row tm-welcome-section">
+                <h2 className="col-12 text-center tm-section-title">Marijo Makang Disini Kawan</h2>
+                <p className="col-12 text-center">Budayakan malas memasa, karena memasa torang pe kerja. Singgah jo, nanti om deng tanta yang ator ngoni pe makang. Mar jang lupa bayar neh, nda mahal-mahal skali kwa eh.</p>
+                </header>
+                <div className="tm-paging-links">
+                    <h1 className="col-12 text-center tm-section-title">Menu Makanan</h1>
+                </div>
+
+                {/* Menu */}
+                <div className="row tm-gallery">
+                {/* gallery page 1 */}
+                <div id="tm-gallery-page-pizza" className="tm-gallery-page">
+                               
+                {
+                    menu.map(item => {
+                        return(
+                             <article className="col-lg-3 col-md-4 col-sm-6 col-20 tm-gallery-item">
+                                <figure>
+                                    <img src={`img/${item.gambar}.jpg`} alt="Image" className="img-fluid tm-gallery-img" />
+                                    <figcaption>
+                                        <h4 className="tm-gallery-title">{item.judul}</h4>
+                                        <p className="tm-gallery-description">{item.keterangan}</p>
+                                        <p className="tm-gallery-price">{item.harga}</p>
+                                    </figcaption>
+                                </figure>
+                            </article>
+                            
+                        )
+                     })
+                }
+                </div> 
+                </div>
+               
+               
+               
+               
+               
+               
+                <div className="tm-section tm-container-inner">
+                <div className="row">
+                    <div className="col-md-6">
+                    <figure className="tm-description-figure">
+                        <img src="https://asset.kompas.com/crops/pjtMmztUVAA5dh3m4aYd289jugo=/0x0:773x515/750x500/data/photo/2022/08/30/630d6bda8bef1.png" alt="Image" className="img-fluid" />
+                    </figure>
+                    </div>
+                    <div className="col-md-6">
+                    <div className="tm-description-box"> 
+                    <br></br>
+                        <h2 style={{color: 'rgba(110, 1, 1, 1)'}}>Terbukti Terkenal!!!</h2> <br></br>
+                        <p className="tm-mb-45">See??? Bahkan Jess No Limit dan Sisca Kohl pernah makang disiniii :p. So, tunggu apalagi guys? Pesan jo sekarang! ^^</p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <br></br><br></br><br></br>
+                <About />
+                <Contact />
+            </main>
+        </div>
+
+        
+
+    )
+};
+
+export default Homepage;
