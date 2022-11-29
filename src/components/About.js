63956b176208a0f4120a81863cd4be6c550d7085
SherrenKalalo <s2200043@student.unklab.ac.id>
2022-11-30 07:09:52 +0800

    tambah about

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import "../assets/css/templatemo-style.css";
 
 export default function About() {
@@ -6,8 +6,9 @@ export default function About() {
     <div id='about'>
         <main>
   <header className="row tm-welcome-section">
-    <h2 className="col-12 text-center tm-section-title">Tentang Warong Kampoeng</h2>
-    <p className="col-12 text-center">Ini rumah makang paling populer di Sulawesi Utara, sejak jaman purba :p. Dpe owner bae-bae, dpe CEO cantik skali, dpe chef jago memasa skali. Marijo bakudapa deng dorang.</p>
+    <h2 className="col-12 text-center tm-section-title">About Simple House</h2>
+    <p className="col-12 text-center">This is about page of simple house template. 
+      You can modify and use this HTML template for your website. Total 3 HTML pages included in this template. Header image has a parallax effect.</p>
   </header>
   <div className="tm-container-inner tm-persons">
     <div className="row">
@@ -15,11 +16,9 @@ export default function About() {
         <figure className="tm-person">
           <img src="img/about-01.jpg" alt="Image" className="img-fluid tm-person-img" />
           <figcaption className="tm-person-description">
-            <h4 className="tm-person-name">Jennifer Soft</h4>
-            <p className="tm-person-title">Founder and CEO</p>
-            <p className="tm-person-about">Vivamus cursus leo nec sem feugiat sagittis.
-              Duis ut feugiat odio, sit amet accumsan
-              odio.</p>
+            <h4 className="tm-person-name">Gregorius  Vincent Adam</h4>
+            <p className="tm-person-title">OWNER OF WARONG KAMPOENG</p>
+            <p className="tm-person-about">Informatics, Computer Science.</p>
             <div>
               <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
               <a href="https://twitter.com" className="tm-social-link"><i className="fab fa-twitter tm-social-icon" /></a>
@@ -32,10 +31,9 @@ export default function About() {
         <figure className="tm-person">
           <img src="img/about-02.jpg" alt="Image" className="img-fluid tm-person-img" />
           <figcaption className="tm-person-description">
-            <h4 className="tm-person-name">Daisy Walker</h4>
-            <p className="tm-person-title">Executive Chef</p>
-            <p className="tm-person-about">Praesent non vulputate elit. Orci varius
-              natoque et magnis dis parturient, nascetur ridiculus mus.</p>
+            <h4 className="tm-person-name">Gabriela Anastasya Lahengke</h4>
+            <p className="tm-person-title">CEO</p>
+            <p className="tm-person-about">Informatics, Computer Science.</p>
             <div>
               <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
               <a href="https://twitter.com" className="tm-social-link"><i className="fab fa-twitter tm-social-icon" /></a>
@@ -47,10 +45,9 @@ export default function About() {
         <figure className="tm-person">
           <img src="img/about-03.jpg" alt="Image" className="img-fluid tm-person-img" />
           <figcaption className="tm-person-description">
-            <h4 className="tm-person-name">Florence Nelson</h4>
-            <p className="tm-person-title">Kitchen Manager</p>
-            <p className="tm-person-about">Aenean sapien sem, ultricies sed vulputate
-              et, auctor vel mauris. Integer sit amet diam eget est facilisis lacinia vitae.</p>
+            <h4 className="tm-person-name">Sherren Brenda Kalalo</h4>
+            <p className="tm-person-title">Chef</p>
+            <p className="tm-person-about">Informatics, Computer Science.</p>
             <div>
               <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
               <a href="https://instagram.com" className="tm-social-link"><i className="fab fa-instagram tm-social-icon" /></a>
@@ -62,10 +59,9 @@ export default function About() {
         <figure className="tm-person">
           <img src="img/about-04.jpg" alt="Image" className="img-fluid tm-person-img" />
           <figcaption className="tm-person-description">
-            <h4 className="tm-person-name">Valentina Martin</h4>
-            <p className="tm-person-title">Culinary Director</p>
-            <p className="tm-person-about">Praesent non vulputate elit. Orci varius
-              natoque penatibus et magnis montes, nascetur ridiculus mus.</p>
+            <h4 className="tm-person-name">Hendra Ucok Luntungan</h4>
+            <p className="tm-person-title">Chef</p>
+            <p className="tm-person-about">Informatics, Computer Science.</p>
             <div>
               <a href="https://fb.com" className="tm-social-link"><i className="fab fa-facebook tm-social-icon" /></a>
               <a href="https://twitter.com" className="tm-social-link"><i className="fab fa-twitter tm-social-icon" /></a>
@@ -111,7 +107,21 @@ export default function About() {
       </div>
     </div>
   </div>
+  <div className="tm-container-inner tm-history">
+    <div className="row">
+      <div className="col-12">
+        <div className="tm-history-inner">
+          <img src="img/about-06.jpg" alt="Image" className="img-fluid tm-history-img" />
+          <div className="tm-history-text"> 
+            <h4 className="tm-history-title">History of our restaurant</h4>
+            <p className="tm-mb-p">Sed ligula risus, interdum aliquet imperdiet sit amet, auctor sit amet justo. Maecenas posuere lorem id augue interdum vehicula. Praesent sed leo eget libero ultricies congue.</p>
+            <p>Redistributing this template as a downloadable ZIP file on any template collection site is strictly prohibited. You will need to <a href="https://templatemo.com/contact">contact TemplateMo</a> for additional permissions about our templates. Thank you.</p>
+          </div>
+        </div>	
+      </div>
+    </div>
+  </div>
         </main>
     </div>
-  );
-};
+  )
+}
