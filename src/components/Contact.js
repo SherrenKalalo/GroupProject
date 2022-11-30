c242cc63c1c88dcefd9c9210bd010bc4dd69dcb0
gabrielalahengke <s2200672@student.unklab.ac.id>
2022-11-30 08:12:03 +0800

    edit file contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import "../assets/css/templatemo-style.css";
+import React from 'react'
+import "../assets/css/templatemo-style.css"
 
 export default function Contact() {
   return (
